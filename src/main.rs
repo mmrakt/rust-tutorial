00d240edf82ba141fd211aa9c27a05ba14803931
mmrakt <mimuraakkun3@gmail.com>
2025-04-28 14:37:01 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Program parsing arguments: {}", err);
-        process::exit(1);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1)
     });
 
     println!("Searching for {}", config.query);
